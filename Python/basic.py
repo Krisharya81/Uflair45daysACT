714fa1cb6955988b19f72b0dc4a25d2714317ee5
Krisharya81 <Krisharya81>
2025-06-14 11:50:27 +0530

    New

--- Python/basic.py
+++ Python/basic.py
@@ -1,7 +1,12 @@
-a = 10
-b = True
-c = "hello"
+# a = 10
+# b = True
+# c = "hello"
 
-print(type(a))
-print(type(b))
-print(type(c))
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+
+def add(a, b):
+    """Adds two numbers together."""
+    return a + b
