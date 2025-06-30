176218dfcea1811316a969c11db20500fef351c2
Krisharya81 <Krisharya81>
2025-06-30 12:01:15 +0530

    New

---
+++ Framwork/app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, request, url_for
+import pandas as pd
+import joblib,pickle
+
+#load the model
+model = pickle.load(open('model.pkl', 'rb'))
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/project")
+def project():
+    return render_template('project.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        try:
+            brand_name = request.form['brand_name']
+            owner_name =int(request.form['owner'])
+            age_bike = int(request.form['age'])
+            power_bike = int(request.form['power'])
+            kms_driven_bike = int(request.form['kms_driven'])
+
+            bike_number = {'TVS': 0,
+                            'Royal Enfield': 1,
+                            'Triumph': 2,
+                            'Yamaha': 3, 
+                            'Honda': 4,
+                            'Hero': 5,
+                            'Bajaj': 6,
+                            'Suzuki': 7,
+                            'Benelli': 8,
+                            'KTM': 9,
+                            'Mahindra': 10,
+                            'Kawasaki': 11,
+                            'Ducati': 12,
+                            'Hyosung': 13,
+                            'Harley-Davidson': 14,
+                            'Jawa': 15,
+                            'BMW': 16,
+                            'Indian': 17,
+                            'Rajdoot': 18,
+                            'LML': 19,
+                            'Yezdi': 20,
+                            'MV': 21,
+                            'Ideal': 22}
+
+                           
+            brand_name_encoded = bike_number.get(brand_name)
+            input_data = [[owner_name,
+                          brand_name_encoded,
+                          kms_driven_bike,
+                          age_bike,
+                          power_bike]]
+        
+            
+
+            prediction = model.predict(input_data)
+            prediction = round(prediction[0],2)
+            return render_template('project.html',prediction=prediction)
+
+        except:
+            return 'something is wrong' 
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- Framwork/st.py
+++ Framwork/st.py
@@ -1,4 +1,5 @@
-# Webpages for ml and data projects
+## streamlit 
+# Webpages for ml and data science projects
 # python lib
 # no requirements of html and css
 import pandas as pd
@@ -11,7 +12,6 @@ import altair as alt
 st.set_page_config(page_title='Streamlit function demo',
                      page_icon='😁',
                      layout='centered',)
-
 
 
 ## title and  text elements
@@ -35,8 +35,6 @@ st.exception(ValueError("This is an exception message"))
 
 st.divider()
 
-
-
 ##data display
 
 st.header("3. Data display")
@@ -59,3 +57,87 @@ fig , ax = plt.subplots()
 ax.plot(df.index,df.a)
 st.pyplot(fig)
 st.divider()
+
+#widgets
+st.header("5. Widgets")
+with st.form("Input Form"):
+    name = st.text_input("Enter your name")
+    age = st.number_input("Enter your age") 
+    mood = st.radio("Select your mood",("happy","sad","angry"))
+    languages = st.multiselect("Select your languages",("English","French","Spanish"))
+    submit = st.form_submit_button("submit")
+    if submit :
+        st.success(f"Name :{name}, Age :{int(age)}, Mood : {mood}, Languages : {languages}" )
+
+
+col1 , col2 , col3 = st.columns([4,1,1])
+with col1:
+    st.text_input("Enter your name")
+    st.number_input("Enter your age")
+with col2:
+    st.radio("Select your mood",("happy","sad","angry"))
+    st.multiselect("Select your languages",("English","French","Spanish"))
+with col3:
+    st.title("Output")    
+
+
+## we can divide from multiple columns
+# for that first define form then define columns
+# with st.form("input form")
+#    col1 , col2 , col3 = st.columns([4,1,1])
+#    with col1:
+#     name = st.text_input("Enter your name")
+#     age = st.number_input("Enter your age") 
+#     mood = st.radio("Select your mood",("happy","sad","angry"))
+#     languages = st.multiselect("Select your languages",("English","French","Spanish"))
+#     submit = st.form_submit_button("submit")
+#     if submit :
+#         st.success(f"Name :{name}, Age :{int(age)}, Mood : {mood}, Languages : {languages}" )
+
+
+
+col1, col2 = st.columns(2)
+with col1:
+    number = st.slider("Select a number ",0,100)
+with col2:
+    colour = st.color_picker("Select a color", "#DC8080")  
+
+
+st.text_area("Enter your text here")
+st.date_input("Select a date")
+st.time_input("Select a time")
+st.file_uploader("Upload a file")
+
+## Media Related
+st.header("6. Media Related")
+st.image("https://picsum.photos/400",caption="random image")
+st.video("https://www.youtube.com/watch?v=9bZkp7q19f0")
+st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
+
+## Sidebar  and layout
+st.sidebar.header("Sidebar")
+st.sidebar.write("Enter your name")
+st.sidebar.button("click me")
+option = st.sidebar.selectbox("select an option",("option 1","option 2","option 3"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## file handling
+## generator and deco
+## multithreading and multiprocessing
+## method overloading and overriding
+## debug and unit testing
+
+
+
